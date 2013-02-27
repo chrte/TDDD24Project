81be47a502fe581b36240e60e823a5df5950e566
chrte707 <chrte707@student.liu.se>
2013-02-27 10:24:39 +0100

    Created a sort of login screen, also have redone the implementation of the entryPoint, created a new class in and placed the mainPage (by @hento581) in a new class. Called on by the new class.

---
+++ src/com/TDDD24Project/client/LoginManager.java
@@ -0,0 +1,53 @@
+package com.TDDD24Project.client;
+
+import com.google.gwt.aria.client.WidgetRole;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * From google tutorial
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+
+
+
+public class LoginManager extends Composite {
+	private TDDD24Project parent;
+	public LoginManager(TDDD24Project parent) {
+//		RootPanel rootPanel = RootPanel.get();
+		this.parent=parent;
+		HorizontalPanel horizontalPanel = new HorizontalPanel();
+//		rootPanel.add(horizontalPanel, 10, 10);
+		horizontalPanel.setSize("470px", "212px");
+		
+		VerticalPanel verticalPanel = new VerticalPanel();
+		horizontalPanel.add(verticalPanel);
+		
+		Label lblWelcomeToThe = new Label("Welcome to my login page");
+		lblWelcomeToThe.setStyleName("gwt-Label-Login");
+		verticalPanel.add(lblWelcomeToThe);
+		lblWelcomeToThe.setSize("206px", "124px");
+		
+		Login login = new Login(this);
+		horizontalPanel.add(login);
+	
+		this.initWidget(horizontalPanel);
+		
+		
+		
+	}
+
+	public void userLoggedIn(int i) {
+		parent.userLoggedIn(i);
+		
+	}
+}
