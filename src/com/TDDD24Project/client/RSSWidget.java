47ecc43c011a923b55049d0c861030acac3258fb
chrte707 <chrte707@student.liu.se>
2013-03-12 17:42:03 +0100

    added dnd for the rss

--- src/com/TDDD24Project/client/RSSWidget.java
+++ src/com/TDDD24Project/client/RSSWidget.java
@@ -1,22 +1,50 @@
 package com.TDDD24Project.client;
 
+import static com.google.gwt.query.client.GQuery.$;
+import gwtquery.plugins.draggable.client.events.BeforeDragStartEvent;
+import gwtquery.plugins.draggable.client.events.BeforeDragStartEvent.BeforeDragStartEventHandler;
+import gwtquery.plugins.draggable.client.events.DragStopEvent;
+import gwtquery.plugins.draggable.client.events.DragStopEvent.DragStopEventHandler;
+import gwtquery.plugins.draggable.client.gwt.DraggableWidget;
+
 import java.util.ArrayList;
 
-import com.TDDD24Project.server.RSSFeedParser;
 import com.TDDD24Project.shared.FeedMessage;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
+import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Widget;
 
 
-public class RSSWidget extends Composite {
+public class RSSWidget extends DraggableWidget<Widget> {
+	interface RSSWidgetUiBinder extends UiBinder<Widget, Portlet> {
+	
+	  }
+	private static class DraggablePositionHandler implements
+    BeforeDragStartEventHandler, DragStopEventHandler {
 
+  /**
+   * before that the drag operation starts, we will "visually" detach the draggable by setting
+   * it css position to absolute. 
+   */
+  public void onBeforeDragStart(BeforeDragStartEvent event) {
+     // "detach" visually the element of the parent
+    $(event.getDraggable()).css("position", "absolute");
+
+  }
+
+  public void onDragStop(DragStopEvent event) {
+    // "reattach" the element
+    $(event.getDraggable()).css("position", "relative").css("top", null).css(
+        "left", null);
+
+  }
+}
+	// This handler is stateless
+	private static DraggablePositionHandler HANDLER = new DraggablePositionHandler();
+	private static RSSWidgetUiBinder uiBinder = GWT.create(RSSWidgetUiBinder.class);
 	
 	int userId;
 	MainPage parent;
@@ -29,7 +57,8 @@ public class RSSWidget extends Composite {
 		this.parent=parent;
 		this.position = position;
 		this.userId=userId;
-		addRSS(url);				
+		addRSS(url);	
+		setup();
 	}
 	
 	
@@ -83,5 +112,14 @@ public class RSSWidget extends Composite {
 		
 	}
 	
-	
+	 private void setup() {
+		    // opacity of the portlet during the drag
+		    setDraggingOpacity(new Float(0.8));
+		    // zIndex of the portlet during the drag
+		    setDraggingZIndex(1000);
+		    // add position handler
+		    addBeforeDragHandler(HANDLER);
+		    addDragStopHandler(HANDLER);
+
+		  }	
 }
