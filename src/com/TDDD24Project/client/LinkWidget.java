2d9e71d62c0e25cec0e0e0451a07c703002e3dd2
chrte707 <chrte707@student.liu.se>
2013-03-12 12:43:34 +0100

    Well, started to fix the dnd. Sort of working, but the old widgets are not

--- src/com/TDDD24Project/client/LinkWidget.java
+++ src/com/TDDD24Project/client/LinkWidget.java
@@ -1,19 +1,66 @@
 package com.TDDD24Project.client;
 
+import static com.google.gwt.query.client.GQuery.$;
+import gwtquery.plugins.draggable.client.events.BeforeDragStartEvent;
+import gwtquery.plugins.draggable.client.events.BeforeDragStartEvent.BeforeDragStartEventHandler;
+import gwtquery.plugins.draggable.client.events.DragStopEvent;
+import gwtquery.plugins.draggable.client.events.DragStopEvent.DragStopEventHandler;
+import gwtquery.plugins.draggable.client.gwt.DraggableWidget;
+
+import com.TDDD24Project.client.Portlet.PortletUiBinder;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbsolutePanel;
-import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Widget;
 
-public class LinkWidget extends Composite {
+public class LinkWidget extends DraggableWidget<Widget> {
+	interface LinkWidgetUiBinder extends UiBinder<Widget, Portlet> {
 
+		
+	  }
+	private static class DraggablePositionHandler implements
+    BeforeDragStartEventHandler, DragStopEventHandler {
+
+  /**
+   * before that the drag operation starts, we will "visually" detach the draggable by setting
+   * it css position to absolute. 
+   */
+  public void onBeforeDragStart(BeforeDragStartEvent event) {
+     // "detach" visually the element of the parent
+    $(event.getDraggable()).css("position", "absolute");
+
+  }
+
+  public void onDragStop(DragStopEvent event) {
+    // "reattach" the element
+    $(event.getDraggable()).css("position", "relative").css("top", null).css(
+        "left", null);
+
+  }
+}
+
+// This handler is stateless
+private static DraggablePositionHandler HANDLER = new DraggablePositionHandler();
+private static LinkWidgetUiBinder uiBinder = GWT.create(LinkWidgetUiBinder.class);
+
+@UiField
+DivElement content;
+@UiField
+DivElement header;
 	int userId;
 	MainPage parent;
 	int position;
 	public LinkWidget(MainPage parent, int userId, int position, String url){
+//		 initWidget(uiBinder.createAndBindUi(this));
+		
+		 setup();
 		this.parent=parent;
 		this.position = position;
 		this.userId=userId;
@@ -64,4 +111,14 @@ public class LinkWidget extends Composite {
 		parent.projectSvc.addWidget(userId, link, position,"link", callback);
 		
 	}
+	 private void setup() {
+		    // opacity of the portlet during the drag
+		    setDraggingOpacity(new Float(0.8));
+		    // zIndex of the portlet during the drag
+		    setDraggingZIndex(1000);
+		    // add position handler
+		    addBeforeDragHandler(HANDLER);
+		    addDragStopHandler(HANDLER);
+
+		  }
 }
--- src/com/TDDD24Project/client/MainPage.java
+++ src/com/TDDD24Project/client/MainPage.java
@@ -2,13 +2,22 @@ package com.TDDD24Project.client;
 
 
 
+import gwtquery.plugins.draggable.client.DraggableOptions.RevertOption;
+import gwtquery.plugins.draggable.client.gwt.DraggableWidget;
+import gwtquery.plugins.droppable.client.DroppableOptions.DroppableTolerance;
+import gwtquery.plugins.droppable.client.gwt.DroppableWidget;
+
 import java.util.ArrayList;
 
 import com.TDDD24Project.shared.WidgetInfo;
+import com.allen_sauer.gwt.dnd.client.DragHandler;
+import com.allen_sauer.gwt.dnd.client.DragHandlerAdapter;
+import com.allen_sauer.gwt.dnd.client.drop.GridConstrainedDropController;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Cursor;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
+import com.google.gwt.query.client.plugins.Widgets;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Button;
@@ -25,12 +34,6 @@ import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
-import com.allen_sauer.gwt.dnd.client.DragController;
-import com.allen_sauer.gwt.dnd.client.DragHandler;
-import com.allen_sauer.gwt.dnd.client.DragHandlerAdapter;
-import com.allen_sauer.gwt.dnd.client.PickupDragController;
-import com.allen_sauer.gwt.dnd.client.drop.GridConstrainedDropController;
-
 
 
 
@@ -46,74 +49,63 @@ public class MainPage extends Composite {
 	private VerticalPanel mainPanel = new VerticalPanel();
 	private HorizontalPanel widgetsPanel = new HorizontalPanel();
 	private Image logo = new Image("images/logo.jpg");
-//	private Image background = new Image("images/background.jpg");
+	//	private Image background = new Image("images/background.jpg");
 	private Image bottomlogo = new Image("images/logo.jpg");
 	private int userId;
-	
-	
-	private GridConstrainedDropController gridConstrainedDropController;
 
-
-	private ArrayList<AbsolutePanel> widgets = new ArrayList<AbsolutePanel>();
-	private ArrayList<FlowPanel> flowPanels = new ArrayList<FlowPanel>();
+	private ArrayList<Widget> widgets = new ArrayList<Widget>();
+	private ArrayList<DroppablePanel> droppablePanels = new ArrayList<DroppablePanel>();
+	//	private ArrayList<FlowPanel> flowPanels = new ArrayList<FlowPanel>();
 
 	protected ProjectServiceAsync projectSvc = GWT.create(ProjectService.class);
 
 	public MainPage(int userId) {
 
-	
-
 		this.userId=userId;
-//		RootPanel.get("main").setStyleName("main");	
+		//		RootPanel.get("main").setStyleName("main");	
 		LoadStandardView();
 		LoadUserData();
-		
-		
-//		addRssWidget(8, "http://www.aftonbladet.se/rss.xml");
+
+
+		//		addRssWidget(8, "http://www.aftonbladet.se/rss.xml");
 
 		initWidget(mainPanel);
 	}
 
 
-		
-		
-
 
 	private void LoadUserData() {
-		// TODO: Implement this?
-		
+
 		addAllUserWidgets();
 
 	}
 
-	//TODO: Remove/change this test function!!
-	
+
 	private void addAllUserWidgets() {
-		
-		
-		
+
+
+
 		AsyncCallback<ArrayList<WidgetInfo>> callback = new AsyncCallback<ArrayList<WidgetInfo>>() {
 			public void onFailure(Throwable caught) {
 				System.out.println("failure");				
 			}
-			
+
 			@Override
 			public void onSuccess(ArrayList<WidgetInfo> result) {
-				
-					AddDataFromDatabase(result);
-			
-				
+
+				AddDataFromDatabase(result);
+
 			}
 
 			private void AddDataFromDatabase(ArrayList<WidgetInfo> result) {	//TODO: Move
 				for(int i=0; i<result.size(); i++){
 					String widgetType = result.get(i).getWidgetType();
 					if(widgetType.equals("link")){
-						
+
 						addLinkWidgetAlreadyInDatabase(positionToIndex(result.get(i).getPosition()), result.get(i).getWidgetData());
 					}
 					else if(widgetType.equals("RSS")){
-						
+
 						addRSSWidgetAlreadyInDatabase(positionToIndex(result.get(i).getPosition()), result.get(i).getWidgetData());
 					}
 					else{	//TODO: Something else???
@@ -121,17 +113,17 @@ public class MainPage extends Composite {
 					}
 				}
 			}
-			
+
 		};
-		
-		
+
+
 		projectSvc.getUsersWidgetData(userId, callback);
-		
-		
+
+
 	}
 
 	private void deleteFromDatabase(int widgetId){
-		
+
 		AsyncCallback<String> callback = new AsyncCallback<String>() {
 			public void onFailure(Throwable caught) {
 				System.out.println("failure");
@@ -139,51 +131,62 @@ public class MainPage extends Composite {
 			@Override
 			public void onSuccess(String result) {
 				System.out.println("success");				
-				
+
 			}
-			
+
 		};
-		
-		
+
 		projectSvc.removeWidget(12, callback);
-		
 	}
-	
-	
+
+
 
 
 	protected void addWidget(int index, String url){ 	//TODO: make more general (add support for RSS etc..)
-		
-		AbsolutePanel tempPanel = (AbsolutePanel) widgets.get(index);
-		tempPanel.clear();
+
+		Widget tempPanel =  widgets.get(index);
+//		tempPanel.clear();
 		final int position = indexToPosition(index);
 		LinkWidget linkWidget = new LinkWidget(this, userId, position, url);
 		linkWidget.addLinkToDatabase(url, userId);
-		tempPanel.add(linkWidget);	
+	
+		tempPanel.setLayoutData(linkWidget);  //TODO, correct??
 		
+
+
 	}
-	
-	
+
+
+
 
 	protected void addLinkWidgetAlreadyInDatabase(int index, String url){
-		
-		AbsolutePanel tempPanel = (AbsolutePanel) widgets.get(index);
-		tempPanel.clear();
+
+		Widget tempPanel =  widgets.get(index);
+//		tempPanel.remove();
 		final int position = indexToPosition(index);
-		tempPanel.add(new LinkWidget(this, userId, position, url));	
-		
+		LinkWidget tempWidget = new LinkWidget(this, userId, position, url);
+		//		DraggableWidget<LinkWidget> tempDragWidget = new DraggableWidget<LinkWidget>(tempWidget);
+		//		tempDragWidget.setDraggingCursor(Cursor.MOVE);
+		//		tempDragWidget.setDraggingZIndex(100);
+
+		droppablePanels.get ((int) Math.floor(index/3.0)).setLayoutData(tempWidget);
+		tempPanel.setLayoutData(tempWidget);	
+
+
 	}
-	
-	
-protected void addRSSWidgetAlreadyInDatabase(int index, String url){
+
+
+	protected void addRSSWidgetAlreadyInDatabase(int index, String url){
+
+		Widget tempPanel =  widgets.get(index);
 		
-		AbsolutePanel tempPanel = (AbsolutePanel) widgets.get(index);
-		tempPanel.clear();
 		final int position = indexToPosition(index);
-		tempPanel.add(new RSSWidget(this, userId, position, url));	
-		
+		Widget rss = new RSSWidget(this, userId, position, url);
+		tempPanel.setLayoutData(rss);	 //TODO, correct??
+		droppablePanels.get ((int) Math.floor(index/3.0)).setLayoutData(rss);
+
 	}
-	
+
 	void addRssWidget(int index, String url){
 		AbsolutePanel tempPanel = (AbsolutePanel) widgets.get(index);
 		tempPanel.clear();
@@ -191,39 +194,35 @@ protected void addRSSWidgetAlreadyInDatabase(int index, String url){
 		RSSWidget rssWidget = new RSSWidget(this, userId, position, url);
 		rssWidget.addRSSToDatabase(url, userId);
 		tempPanel.add(rssWidget);
-		
-		
+
+
 	}
-	
-	
+
+
 	private void addAlertWidget(int index){
 		AbsolutePanel tempPanel = (AbsolutePanel) widgets.get(index);
 		tempPanel.clear();
 		tempPanel.add(new AlertWidget());	
 	}
-	
+
 	private void removeWidget(int column, int row, int userId){
 		int removePos = column*10+row;
-		
+
 		//TODO: Fix
 
 	}
 
 	private Widget createEmptyWidget(final int index){
-		
-
 
 		Image plusSign = new Image("images/plus_sign.png");
 		plusSign.setPixelSize(160, 160);
-
-
 		plusSign.addClickHandler(new ClickHandler(){
 
 			@Override
 			public void onClick(ClickEvent event) {
 
 				chooseNewWidget(index);
-				
+
 			}
 
 		});
@@ -233,26 +232,26 @@ protected void addRSSWidgetAlreadyInDatabase(int index, String url){
 	}
 
 	private int indexToPosition(int index) {	//TODO: Can this be done more general if we add more fields???
-		
-			
+
+
 		int x = (int) Math.floor(index/3.0+1);
 		int y = index%(3)+1;
-		
+
 		int position = x+10*y;
-		
+
 		return position;
 	}
-	
-	
+
+
 	private int positionToIndex(int position){ //TODO: test, make more general?
-		
+
 		int one = (int) Math.floor(position/10)-1;
 		int ten = position-(one+1)*10;
-		
+
 		int tenIsWorth = (ten-1)*3;
 
 		int index = tenIsWorth+one;
-					
+
 		return index;
 	}
 
@@ -279,7 +278,7 @@ protected void addRSSWidgetAlreadyInDatabase(int index, String url){
 
 				String url = widgetLink.getText();
 				addWidget(index, url);
-//				addAlertWidget(index);
+				//				addAlertWidget(index);
 				chooseWidget.hide();
 			}
 		};
@@ -296,60 +295,76 @@ protected void addRSSWidgetAlreadyInDatabase(int index, String url){
 		popUpPanelContents.add(widgetLink);
 		popUpPanelContents.add(holder);
 		popUpPanelContents.add(holder2);
-		
+
 		chooseWidget.setWidget(popUpPanelContents);
 		chooseWidget.setGlassEnabled(true);
 		chooseWidget.center();
 	}
-		
+
 
 	private void LoadStandardView(){
 
 		//		widgetColumn1.getElement().getStyle().setProperty("margin", "10px");
 
 
-		ArrayList<Image> plusSigns = new ArrayList<Image>();
+		ArrayList<DraggableWidget<Image>> plusSigns = new ArrayList<DraggableWidget<Image>>();
 
-		for(int i=0;i<9;i++){
-			AbsolutePanel tempPanel = new AbsolutePanel();
-			tempPanel.getElement().getStyle().setProperty("margin", "10px");
-			
-			
-			final DragHandler demoDragHandler = new DragHandlerAdapter();
+		int i = 0;
+		//		AbsolutePanel tempPanel = new AbsolutePanel();
+		//		tempPanel.getElement().getStyle().setProperty("margin", "10px");
+		//
+		//		final DragHandler demoDragHandler = new DragHandlerAdapter();			
+				Image plusSign = (Image) createEmptyWidget(i);
 
+		droppablePanels.add(new DroppablePanel());
+		Widget portlet = new Portlet("testHeader"+i++, "testin");
+		droppablePanels.get(0).add(portlet);
+		widgets.add(portlet);
+		
+		
+		portlet = new Portlet("testHeader"+i++, "testin");
+		droppablePanels.get(0).add(portlet);
+		widgets.add(portlet);
+		
+		
+		portlet = new Portlet("testHeader"+i++, "testin");
+		droppablePanels.get(0).add(portlet);
+		widgets.add(portlet);
+		widgetsPanel.add(droppablePanels.get(0));
 
-	
-			
-			Image plusSign = (Image) createEmptyWidget(i);
-			plusSigns.add(plusSign);
-			tempPanel.add(plusSigns.get(i));
-			widgets.add(tempPanel);
-			
-			
-		}
+		
+		
+		droppablePanels.add(new DroppablePanel());
+		portlet = new Portlet("testHeader"+i++, "testin");
+		droppablePanels.get(1).add(portlet);
+		widgets.add(portlet);
+		portlet = new Portlet("testHeader"+i++, "testin");
+		droppablePanels.get(1).add(portlet);
+		widgets.add(portlet);
+		portlet = new Portlet("testHeader"+i++, "testin");
+		droppablePanels.get(1).add(portlet);
+		widgets.add(portlet);
+		widgetsPanel.add(droppablePanels.get(1));
+		
 
+		droppablePanels.add(new DroppablePanel());
+		portlet = new Portlet("testHeader"+i++, "testin");
+		droppablePanels.get(2).add(portlet);
+		widgets.add(portlet);
+		portlet = new Portlet("testHeader"+i++, "testin");
+		droppablePanels.get(2).add(portlet);
+		widgets.add(portlet);
+		portlet = new Portlet("testHeader"+i++, "testin");
+		droppablePanels.get(2).add(portlet);
+		widgets.add(portlet);
+		widgetsPanel.add(droppablePanels.get(2));
 
-		for(int i=0;i<3;i++){
-			flowPanels.add(new FlowPanel());
-		}
-
-
-
-
-		for(int i=0;i<3;i++){
-			for(int j=0; j<3; j++ ){
-				flowPanels.get(i).getElement().getStyle().setProperty("margin", "50px");
-				flowPanels.get(i).add(widgets.get(j+3*i));					
-			}
-			widgetsPanel.add(flowPanels.get(i));
-		}
 
 		logo.setPixelSize(1024, 60);
 		bottomlogo.setPixelSize(1024, 60);
 		mainPanel.add(logo);
 		mainPanel.add(widgetsPanel);
 		mainPanel.add(bottomlogo);
-
 		RootPanel.get("main").add(mainPanel);	
 
 	}
